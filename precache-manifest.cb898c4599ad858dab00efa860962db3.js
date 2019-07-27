9f33b9b366ff1dd8b50fd1b695d536fcf43a5d79
Moshe Melamed <melamed2@gmail.com>
2019-07-27 17:30:03 +0300

    Updates

---
+++ precache-manifest.cb898c4599ad858dab00efa860962db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be4b3cb0ce0f77cfb9b27fc9d551b7da",
+    "url": "/Moshe-Melamed-18-7-2019/index.html"
+  },
+  {
+    "revision": "f420a168e0f7042bb6e2",
+    "url": "/Moshe-Melamed-18-7-2019/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "142bf6ab06f4eb4c9cff",
+    "url": "/Moshe-Melamed-18-7-2019/static/css/main.9f46cbdc.chunk.css"
+  },
+  {
+    "revision": "f420a168e0f7042bb6e2",
+    "url": "/Moshe-Melamed-18-7-2019/static/js/2.3bf61658.chunk.js"
+  },
+  {
+    "revision": "142bf6ab06f4eb4c9cff",
+    "url": "/Moshe-Melamed-18-7-2019/static/js/main.952d0dc3.chunk.js"
+  },
+  {
+    "revision": "6823fb022a15fa5877c1",
+    "url": "/Moshe-Melamed-18-7-2019/static/js/runtime~main.7dadb793.js"
+  }
+]);
